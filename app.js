fde967b33e038065a16a60c131236e7efb9359c2
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2016-01-10 23:32:32 +0800

    Create posts template, faking comment data, create single post template (show page) & add comment form inside the post template (show page)

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 			url: '/home',
 			templateUrl: '/home.html',
 			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/:id',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
 		});
 
 	$urlRouterProvider.otherwise('home');
@@ -26,7 +31,11 @@ app.controller("MainCtrl", ["$scope", "postsFactory", function($scope, postsFact
 		$scope.posts.push({
 			title: $scope.title,
 			link: $scope.link,
-			upvotes: 4
+			upvotes: 4,
+			comments: [
+				{author: 'Joe', body: 'Hello!', upvotes: 0},
+				{author: 'Bob', body: 'Hi!', upvotes: 0}
+			]
 		});
 
 		// clear the input
@@ -40,6 +49,27 @@ app.controller("MainCtrl", ["$scope", "postsFactory", function($scope, postsFact
 
 }]);
 
+app.controller("PostsCtrl", ["$scope", "$stateParams", "postsFactory", function($scope, $stateParams, postsFactory){
+	// grab the right post from postsFactory posts array
+	$scope.post = postsFactory.posts[$stateParams.id];
+	console.log($scope.post);
+
+	$scope.addComment = function(){
+	  // stop this function if comment body is empty
+	  if($scope.body === '') {
+	  	return;
+	  }
+	  $scope.post.comments.push({
+	    body: $scope.body,
+	    author: 'user',
+	    upvotes: 0
+	  });
+
+	  // clear the input field
+	  $scope.body = '';
+	};
+}]);
+
 app.factory("postsFactory", [function(){
 	/*
 	creating a new object that has an array property called 'posts'.
@@ -48,9 +78,30 @@ app.factory("postsFactory", [function(){
 	*/
 	var obj = {
 		posts: [
-			{title: 'post 1', upvotes: 1},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 3}
+			{
+				title: 'post 1',
+				upvotes: 1,
+				comments: [
+					{author: 'Joe', body: 'Hello!', upvotes: 0},
+					{author: 'Bob', body: 'Hi!', upvotes: 0}
+				]
+			},
+			{
+				title: 'post 2',
+				upvotes: 2,
+				comments: [
+					{author: 'Joe', body: 'Hello!', upvotes: 0},
+					{author: 'Bob', body: 'Hi!', upvotes: 0}
+				]
+			},
+			{
+				title: 'post 3',
+				upvotes: 3,
+				comments: [
+					{author: 'Joe', body: 'Hello!', upvotes: 0},
+					{author: 'Bob', body: 'Hi!', upvotes: 0}
+				]
+			}
 		]
 	};
 	return obj;
